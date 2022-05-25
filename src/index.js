9a12c525a87010fce70e2bfdf51b4a7a07668df1
Jeremy book <jeremy.b@live.com.sg>
2022-05-25 23:07:06 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Auth from './components/auth';
+import reportWebVitals from './reportWebVitals';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+
+function Router(){
+
+  return (
+    
+    <React.StrictMode>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Auth/>}/>
+          <Route exact path="/movies" element={<App/>}/>
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
+  </React.StrictMode>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Router />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
